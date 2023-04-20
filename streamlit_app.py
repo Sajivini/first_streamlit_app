e8bb7b1e9adaa09f9000e63534a2aead1dddc9ff
Sajivini <66542627+Sajivini@users.noreply.github.com>
2023-04-20 19:37:39 +0530

    Update streamlit_app.py
    adding new menus

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title("My parents new health Dinner")
 streamlit.header("Breakfast Meanu")
-streamlit.text("Fruits and fresh juice")
-streamlit.text("Omega 3 & bluberry oatmeal")
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
